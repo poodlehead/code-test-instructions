7390a7187daa0d703d0f8e0452438db4e1b25085
andrewrae5-nhs <andyrrae@gmail.com>
2026-02-02 14:34:15 +0000

    added EF migrations and upgraded to .Net 10

--- backend/UrlShortnerAPI/Data/Data/ShortenedUrl.cs
+++ backend/UrlShortnerAPI/Data/Data/ShortenedUrl.cs
@@ -1,14 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Data.Data
 {
     public class ShortenedUrl
     {
+        [Key]
         public required string shortString { get; set; }
         public required string Url { get; set; }
     }
---
+++ backend/UrlShortnerAPI/Data/Migrations/20260202142532_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShortenedUrls",
+                columns: table => new
+                {
+                    shortString = table.Column<string>(type: "TEXT", nullable: false),
+                    Url = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShortenedUrls", x => x.shortString);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortenedUrls_shortString",
+                table: "ShortenedUrls",
+                column: "shortString",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShortenedUrls");
+        }
+    }
+}
